30ca8e71ee1d6fc870c1931bbdbea66fa8ea9800
Rhyan Inacio <rhyanalves68@gmail.com>
2021-04-20 10:51:31 -0300

    modify in view

--- SoftwareLedStripArduino/src/softwareledstriparduino/SoftwareLedStripArduino.java
+++ SoftwareLedStripArduino/src/softwareledstriparduino/SoftwareLedStripArduino.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +24,9 @@ public class SoftwareLedStripArduino extends Application {
 
         Scene scene = new Scene(root);
 
+        stage.setResizable(false);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/view/rainbow.png")));
+        stage.setTitle("LED Strip Monitor!");
         stage.setScene(scene);
         stage.show();
     }
